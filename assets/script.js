4d0db517806b337c7e0f9bdf3ee6596603cee1e0
Nadine <nadine.mohsen@hotmail.com>
2022-01-11 17:26:37 +0000

    quiz-done

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // Creating an object called questions including all the questions, choices and correct answer
 var questions = [
-    {question:"Commonly used data types DO NO INCLUDE: ",
+    {question:"Commonly used data types DO NOT INCLUDE: ",
     choices: ["strings","booleans","alerts","numbers"],
     answer: "alerts" },
 
@@ -29,14 +29,18 @@ var paragraph= document.getElementById("p1")
 var title=document.querySelector("h1")
 var choicesList=document.getElementById("choices-list")
 var correct = document.getElementById("correct-wrong")
+var timeContainer= document.getElementById("time")
 
 var timer;
 var timerCount=75;
-var penalty=10;
+
 var userQuestion;
 var userChoices;
 var listItem;
 var qindex=0;
+
+var element;
+var score=0;
 
 // Attach event listener to start button to call startGame function on click
 button1.addEventListener("click", startGame)
@@ -61,6 +65,7 @@ function startTimer(){
   clearInterval(timer);
   // loseGame();
   timeLeft.textContent= "Time's up";
+
 }
 },1000)
 }
@@ -75,49 +80,55 @@ function displayQuiz(qIndex){
   for (var i=0;i<questions.length;i++){
     userQuestion = questions[qindex].question;
     userChoices=questions[qindex].choices;
+    //display question
     title.textContent=userQuestion;
   }
+  //display choices
   userChoices.forEach(function(newItem) {
     listItem=document.createElement("li");
     listItem.textContent= newItem;
     choicesList.appendChild(listItem);
-    // listItem.addEventListener("click",compare)
+    listItem.addEventListener("click",compare)
   });
-  
 }
 
-// qindex++;
+//Compare choice with correct answer if the user clicked on one of the choices
+function compare(event){
+  element = event.target;
+  if (element.matches("li")){
+    //correct answer
+    if(element.textContent==questions[qindex].answer){
+      score++;
+      correct.textContent = "Correct Answer "
+    }
+    //wrong answer
+    else {
+      timerCount = timerCount-10;
+      correct.textContent = "Wrong Answer "
+    }
+   
+  }
+  choicesList.innerHTML=""
+  qindex++;
+  next()
+}
+
+function next(){
+  if(qindex>=questions.length){
+    title.innerHTML="Your final score is: "+ timerCount; 
+   endofGame()
+  }
+  else{
+    displayQuiz(qindex)
+  }
+}
+
+function endofGame(){
+  timeContainer.innerHTML=""
+  correct.textContent="End of quiz, your score is: "+ score+"/"+ questions.length;
+  choicesList.innerHTML=""
+}
 
 
-
-
-
-
-
-
-
-// function loseGame(){
-//   correct.textContent="Wrong"
-
-// }
-
-// function winGame(){
-//   correct.textContent="Correct"
-// }
-
-
-// //displays quiz choices
-// function displayChoices(){
-//   quizContainer.setAttribute("style","visibility:visible")
-//   button1.setAttribute("style","visibility:hidden")
-//   for(var i=0; i<answers.ans1.length;i++){
-//     quizContainer.children[i].children[0].textContent=answers.ans1[i];
-//   }
-//   userChoice();
-// }
-
-// function userChoice(){
- 
-// }
 
 
